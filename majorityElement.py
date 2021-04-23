746c6667a64747881b56555634a54871cdd62f2c
Shashiraj Walsetwar <31354536+ridersw@users.noreply.github.com>
2021-04-23 10:46:08 +0530

    Add files via upload

---
+++ majorityElement.py
@@ -0,0 +1,21 @@
+def majorityElement(nums):
+	tempNum = set(nums)
+	return max(tempNum, key = nums.count)
+	
+if __name__ == "__main__":
+	nums = [2,2,1,1,1,2,2]
+	print(majorityElement(nums))
+	
+#Given an array nums of size n, return the majority element.
+
+#The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+#Example 1:
+
+#Input: nums = [3,2,3]
+#Output: 3
+#Example 2:
+
+#Input: nums = [2,2,1,1,1,2,2]
+#Output: 2
