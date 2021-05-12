0e9f6a52b746c30e080f4e3a16d6196a32352e5f
Shashiraj Walsetwar <31354536+ridersw@users.noreply.github.com>
2021-05-12 09:50:17 +0530

    Add files via upload

---
+++ maxScoreCards.py
@@ -0,0 +1,78 @@
+def maxScore(cardPoints, k):
+	#if not k or not cardPoints:
+	#	return 0
+	
+	#length = len(cardPoints)
+	
+	#if len(cardPoints) == k:
+	#	return sum(cardPoints)
+		
+	#length = len(cardPoints)
+	#print(f"cardPoints[:k]: {cardPoints[:k]}")
+	#print(f"cardPoints[k-length+1:length]: {cardPoints[length-k:]}")
+	#endingSum = cardPoints[0] + sum(cardPoints[n-k:])
+	#return max(sum(cardPoints[:k]), sum(cardPoints[length-k:]), (cardPoints[0] + cardPoints[-1] + cardPoints[-2]), (cardPoints[-1] + cardPoints[0] + cardPoints[1]))
+	
+	
+	
+	n = len(cardPoints)
+	
+	sum1 = 0
+	sum2 = sum(cardPoints[n-k:])
+	
+	maxSum = sum2
+	for swi in range(k):
+	
+		sum1 += cardPoints[swi]
+		sum2 -= cardPoints[n-k+swi]
+		maxSum = max(sum1 + sum2, maxSum)
+	return maxSum
+	
+	
+if __name__ == "__main__":
+	#print(maxScore(cardPoints = [1,2,3,4,5,6,1], k = 3))
+	#print(maxScore(cardPoints = [2,2,2], k = 2))
+	#print(maxScore(cardPoints = [9,7,7,9,7,7,9], k = 7))
+	#print(maxScore(cardPoints = [1,1000,1], k = 1))
+	#print(maxScore(cardPoints = [1,79,80,1,1,1,200,1], k = 3))
+	print(maxScore(cardPoints = [100,40,17,9,73,75], k = 3))
+	
+	
+#There are several cards arranged in a row, and each card has an associated number of points The points are given in the integer array cardPoints.
+
+#In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+#Your score is the sum of the points of the cards you have taken.
+
+#Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+#Example 1:
+
+#Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+#Output: 12
+#Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+#Example 2:
+
+#Input: cardPoints = [2,2,2], k = 2
+#Output: 4
+#Explanation: Regardless of which two cards you take, your score will always be 4.
+#Example 3:
+
+#Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+#Output: 55
+#Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+#Example 4:
+
+#Input: cardPoints = [1,1000,1], k = 1
+#Output: 1
+#Explanation: You cannot take the card in the middle. Your best score is 1. 
+#Example 5:
+
+#Input: cardPoints = [1,79,80,1,1,1,200,1], k = 3
+#Output: 202
+ 
+#Constraints:
+
+#1 <= cardPoints.length <= 10^5
+#1 <= cardPoints[i] <= 10^4
+#1 <= k <= cardPoints.length
