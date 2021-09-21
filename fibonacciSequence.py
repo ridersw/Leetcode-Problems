cdc18d5dead666c46e6913340082fde9f39cadf1
Shashiraj Walsetwar <wshashiraj@gmail.com>
2021-09-20 23:34:31 -0700

    Daily Dose: Dynamic Programming- Fibonacci

---
+++ fibonacciSequence.py
@@ -0,0 +1,20 @@
+class Solution:
+	def fib(self, n, memo = {}):
+		if n == 0:
+			return 0
+	
+		if n <= 2:
+			return 1
+		
+		if n in memo:
+			return memo[n]
+			
+		memo[n] = self.fib(n-1) + self.fib(n-2)
+		return memo[n]	
+		
+if __name__ == "__main__":
+	n = 7
+
+	obj = Solution()
+
+	print(obj.fibo(n))	
