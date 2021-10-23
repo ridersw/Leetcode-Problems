732c14079a1957b4002918fb77a0d3fe36422051
Shashiraj Walsetwar <wshashiraj@Shashirajs-MacBook-Air.local>
2021-10-22 22:44:48 -0700

    daily dose

---
+++ robber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def robber(self, houses):
+        dp = [0] * len(houses)
+
+        dp[0] = houses[0]
+        dp[1] = max(houses[0], houses[1])
+
+        for swi in range(2, len(houses)):
+            dp[swi] = max(dp[swi-2] + houses[swi], dp[swi-1])
+
+        return max(dp[-1], dp[-2])
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    houses = [1,2,3,1]
+
+    print(obj.robber(houses))
