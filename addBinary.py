f7d0c75e0f563c02a8edc9ea7da34c3247be4226
ridersw <wshashiraj@gmail.com>
2022-01-10 20:53:07 +0530

    Daily Code

---
+++ addBinary.py
@@ -0,0 +1,31 @@
+class Solution:
+    def addBinary(self, a, b):
+        return format((int(a, 2) + int(b, 2)), 'b')
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    a = "11"
+    b = "1"
+
+    print(obj.addBinary(a, b))
+
+# Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+# Example 1:
+
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+ 
+
+# Constraints:
+
+# 1 <= a.length, b.length <= 104
+# a and b consist only of '0' or '1' characters.
+# Each string does not contain leading zeros except for the zero itself.
