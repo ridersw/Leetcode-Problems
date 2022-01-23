6a59d856153413199b7af1cd30a1eb286df99498
ridersw <wshashiraj@gmail.com>
2022-01-22 23:25:55 -0800

    Daily Code

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -23,8 +23,6 @@ class Solution:
                 
         return room
 
-        
-
 # Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
 
 # Example 1:
