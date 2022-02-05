16fd4d4b1eef19e42429d5a336f5297fcb154bc1
ridersw <wshashiraj@gmail.com>
2022-02-04 21:27:27 -0800

    Daily Code

---
+++ summation.java
@@ -0,0 +1,20 @@
+public class summation{
+
+    public static int getSummation(int[] array){
+        int answer = 0;
+
+        int len = array.length;
+
+        for(int swi=0;swi<len;swi++){
+            answer += array[swi];
+        }
+
+        return answer;
+    }
+    public static void main(String[] args){
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+        int answer = getSummation(array);
+        System.out.println("Answer: " + answer);
+    }
+}
