3a78d07e8459c351e8fdfdea189551d0b7f3e172
ridersw <wshashiraj@gmail.com>
2022-02-09 09:59:30 -0800

    Daily Code

---
+++ searchMatrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        
+        for swi in range(len(matrix)):
+            if target in matrix[swi]:
+                return True
+
+        return False
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    print(obj.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3))
+    print(obj.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13))
+
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+ 
+
+# Example 1:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+# Example 2:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Output: false
+ 
+
+# Constraints:
+
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 100
+# -104 <= matrix[i][j], target <= 104
