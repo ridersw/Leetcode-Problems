a175488984817770235d82043c3115060d83982f
ridersw <wshashiraj@gmail.com>
2022-02-24 12:08:14 -0800

    Daily Code

---
+++ sortLinkedList.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        arr = []
+        
+        temp = head
+        
+        while temp:
+            arr.append(temp.val)
+            temp = temp.next
+            
+        if not arr:
+            return None
+            
+        # print(f'arr: {arr}')
+        arr.sort()
+        
+        res = first = ListNode(arr[0])
+        
+        for swi in range(1, len(arr)):
+            res.next = ListNode(arr[swi])
+            res = res.next
+            
+        res.next = None
+        return first
+
+# Given the head of a linked list, return the list after sorting it in ascending order.
+
+# Example 1:
+
+
+# Input: head = [4,2,1,3]
+# Output: [1,2,3,4]
+# Example 2:
+
+
+# Input: head = [-1,5,3,4,0]
+# Output: [-1,0,3,4,5]
+# Example 3:
+
+# Input: head = []
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 5 * 104].
+# -105 <= Node.val <= 105
