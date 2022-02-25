7e9618e7216913f8728cda5699f98f289cb1b655
ridersw <wshashiraj@gmail.com>
2022-02-24 20:26:09 -0800

    Daily Code

---
+++ removeZeroSumSublists.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeZeroSumSublists(self, nums):
+        
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    print(obj.removeZeroSumSublists(array= [1, 2, -3, 3, 1]))
