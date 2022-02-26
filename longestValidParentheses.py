471dbbd26bb27aa6e521e2454791717bf78cfad6
ridersw <wshashiraj@gmail.com>
2022-02-25 16:43:35 -0800

    Daily Code

--- longestValidParentheses.py
+++ longestValidParentheses.py
@@ -28,8 +28,6 @@ if __name__ == "__main__":
 
 # Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
 
- 
-
 # Example 1:
 
 # Input: s = "(()"
