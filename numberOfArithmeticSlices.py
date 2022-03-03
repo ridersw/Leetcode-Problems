e995b7713c06176e6c3c77783bba1ed6d91f93a8
ridersw <wshashiraj@gmail.com>
2022-03-03 13:34:14 -0800

    Daily Code

--- numberOfArithmeticSlices.py
+++ numberOfArithmeticSlices.py
@@ -3,26 +3,15 @@
 
 class Solution:
     def numberOfArithmeticSlices(self, nums):
-        result = []
-
-        eleCount = 3
-
-        while eleCount <= len(nums):
-            for swi in range(len(nums)-eleCount+1):
-                print(f'nums[swi:eleCount]: {nums[swi:swi+eleCount]}')
-                tempArr = []
-                for swj in range(swi+1, swi+eleCount):
-                    print(f'subtracting {nums[swi]} and {nums[swi-1]}')
-                    tempArr.append(abs(nums[swj]-nums[swj-1]))
-                print(f'tempArr: {tempArr} and tempArr: {tempArr} len(set(tempArr)): {len(set(tempArr))}')
-                if len(set(tempArr)) == 1 and nums[swi:swi+eleCount] not in result:
-                    result.append(nums[swi:swi+eleCount])
-
-
-            eleCount += 1
-
-        print(f'result: {result}')
-        return len(result)
+        le = len(nums)
+        l = [0] * le
+        
+        for swi in range(2, le):
+            if nums[swi] - nums[swi-1] == nums[swi-1] - nums[swi-2]:
+                l[swi] = 1 + l[swi-1]
+                
+        print(l)
+        return sum(l)
 
 
 if __name__ == "__main__":
