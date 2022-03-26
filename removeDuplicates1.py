73f85e1f2f4ee58e7d8f0099fe2ee301d6fd8772
ridersw <wshashiraj@gmail.com>
2022-03-26 09:52:37 -0700

    Daily Code

--- removeDuplicates1.py
+++ removeDuplicates1.py
@@ -13,3 +13,20 @@ if __name__ == "__main__":
     obj = Solution()
 
     print(obj.removeDuplicateLetters(s = "bcabc"))
+
+# Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+# Example 1:
+
+# Input: s = "bcabc"
+# Output: "abc"
+# Example 2:
+
+# Input: s = "cbacdcbc"
+# Output: "acdb"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of lowercase English letters.
