5937e11a458d613cbb0d3cdd646c462078591d48
ridersw <wshashiraj@gmail.com>
2022-03-29 14:13:34 -0700

    Daily Code

--- isInterLeave.py
+++ isInterLeave.py
@@ -5,6 +5,7 @@ class Solution:
         while pointer1 < len(s1) or pointer2 < len(s2) and pointer3 < len(s3):
             print(f'Checking for Character: {s3[pointer3]} at location: {pointer3} s1: {s1[pointer1:]} s2: {s2[pointer2:]}')
             if pointer1 < len(s1) and s1[pointer1] == s3[pointer3]:
+                print(f'Character ')
                 pointer1 += 1
                 pointer3 += 1
             elif pointer2 < len(s2) and s2[pointer2] == s3[pointer3]:
@@ -19,3 +20,5 @@ if __name__ == "__main__":
     obj = Solution()
 
     print(obj.isInterleave(s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"))
+
+    
