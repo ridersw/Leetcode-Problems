3b22ae5cac9670f251fb3073c760e88521e2a3be
ridersw <wshashiraj@gmail.com>
2022-04-02 22:05:29 -0700

    Daily Code

--- multiply.py
+++ multiply.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
 
 # Given two sparse matrices mat1 of size m x k and mat2 of size k x n, return the result of mat1 x mat2. You may assume that multiplication is always possible.
 
- 
 
 # Example 1:
 
