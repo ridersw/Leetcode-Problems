1f889376eac7c8ac42aa0fd09d70c2654e2b6e3d
ridersw <wshashiraj@gmail.com>
2022-04-30 10:51:07 -0700

    Daily Code

--- placeTowers.py
+++ placeTowers.py
@@ -22,3 +22,5 @@ class Solution:
 if __name__ == "__main__":
     obj = Solution()
     print(obj.placeTowers(locations = [10, 45, 34, 12, 4, 6, 14, 19 ], range = 4))
+
+#place towers so that every house gets covered
