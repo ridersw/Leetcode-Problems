1109d9ed962918237d4479ae1413cc7e8ef22817
ridersw <wshashiraj@gmail.com>
2022-04-30 10:50:24 -0700

    Daily Code

---
+++ placeTowers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def placeTowers(self, locations, range):
+        locations.sort()
+        print(f'locations: {locations}')
+
+        pointer = 0
+        towerLocations = []
+
+        while pointer < len(locations):
+            ref = locations[pointer]
+            while pointer < len(locations) and locations[pointer] <= ref + range:
+                pointer += 1
+
+            towerLocations.append(locations[pointer-1])
+
+            ref = towerLocations[-1]
+            while pointer < len(locations) and locations[pointer] <= ref + range:
+                pointer += 1
+
+        print(f'towerLocations: {towerLocations}')
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.placeTowers(locations = [10, 45, 34, 12, 4, 6, 14, 19 ], range = 4))
