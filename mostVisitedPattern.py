81965a2ed9823d6ba3c98f01c81e6207e91b0bb0
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-05-17 22:11:21 -0700

    Daily Code

---
+++ mostVisitedPattern.py
@@ -0,0 +1,11 @@
+from typing import DefaultDict
+
+
+class Solution:
+    def subArrayRanges(self, nums):
+        
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.subArrayRanges(nums = [1,2,3]))
