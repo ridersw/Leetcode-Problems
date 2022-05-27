172eebd67b4866faea4b9707c79e7d2536020d51
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-05-26 20:44:37 -0700

    Daily Code

--- numberOfSteps.py
+++ numberOfSteps.py
@@ -13,7 +13,6 @@ class Solution:
         return count
 
 # Given an integer num, return the number of steps to reduce it to zero.
-
 # In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 
 # Example 1:
