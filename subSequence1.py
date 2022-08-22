a058a8209be0a1fff0763e50f5acb71ebe8134c3
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-08-22 13:39:30 -0700

    Daily Code

---
+++ subSequence1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pointer1 = 0
+        pointer2 = 0
+        
+        while pointer1 < len(s) and pointer2 < len(t):
+            if s[pointer1] == t[pointer2]:
+                pointer1 = pointer1 + 1
+                pointer2 = pointer2 + 1
+            else:
+                pointer2 = pointer2 + 1
+                
+        return pointer1 == len(s)
+
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 100
+# 0 <= t.length <= 104
+# s and t consist only of lowercase English letters.
+ 
+
+# Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
