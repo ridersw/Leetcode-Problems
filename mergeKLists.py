d1c6b227b6226480463aa321c308b6f449e72851
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-10-22 10:06:20 -0700

    Daily Code

---
+++ mergeKLists.py
@@ -0,0 +1,67 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        
+        mainList = lists[0]
+        
+        for swi in range(0, len(lists)-1):
+            list2 = lists[swi+1]
+            mainList = self.mergLists(mainList, list2)
+            
+        return mainList
+        
+        
+    def mergLists(self, list1, list2):
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergLists(list1.next, list2)
+            return list1
+        else: #list1.val > list2.val
+            list2.next = self.mergLists(list1, list2.next)
+            return list2
+
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+# Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+# Example 1:
+
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+# Example 2:
+
+# Input: lists = []
+# Output: []
+# Example 3:
+
+# Input: lists = [[]]
+# Output: []
+ 
+
+# Constraints:
+
+# k == lists.length
+# 0 <= k <= 104
+# 0 <= lists[i].length <= 500
+# -104 <= lists[i][j] <= 104
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length will not exceed 104.
