048a460297d04886fdc2540823aa426713c2dc55
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-11-03 13:49:06 -0700

    Daily Code

--- birdForest.py
+++ birdForest.py
@@ -7,14 +7,11 @@ class Solution:
         
 
         while True:
-            print(f'Left: {left} and Right: {right}')
             if temp_sum <= 0:
                 break
             
             while forest[right] == 0:
                 right += 1
-            
-            print(f'right: {right}')
             res.append(right)
             temp_sum -= forest[right]
             right += 1
@@ -27,7 +24,6 @@ class Solution:
             while forest[left] == 0:
                 left -= 1
             
-            print(f'left: {left}')
             res.append(left)
             temp_sum -= forest[left]
             left -= 1
@@ -41,6 +37,9 @@ if __name__ == "__main__":
     obj = Solution()
 
     forest = [25, 0, 50, 0, 0,0,0,15, 0,0,45]
+    bird = 4
+
+    forest = []
     bird = 4
 
     print(obj.program(forest, bird))
