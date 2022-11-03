e35f2c79bf0c572b06fd213bd71fb628ca211c39
Shashiraj Walsetwar <wshashiraj@gmail.com>
2022-11-03 13:49:38 -0700

    Daily Code

---
+++ stringTandS.py
@@ -0,0 +1,21 @@
+class Solution:
+    def program(self, t, s):
+        if len(t) < 5:
+            return 0
+
+        count = 0
+        s = list(s)
+
+        for swi in range(len(t)-2):
+            if t[swi] == s[0] and t[swi+2] == s[1] and t[swi+4] == s[2]:
+                count += 1
+
+        return count
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    t = "azcabcab"
+    s = "acb"
+
+    print(obj.program(t, s))
