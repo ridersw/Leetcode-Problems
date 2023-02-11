0c24e5eeb2b70a0f04a15b2f282c4264b0534392
Shashiraj Walsetwar <wshashiraj@gmail.com>
2023-02-11 00:32:08 -0800

    Daily Code

--- vapr_rte.py
+++ vapr_rte.py
@@ -6,6 +6,7 @@ class Solution:
 
 if __name__ == "__main__":
     obj = Solution()
-    print(obj.function(vapor_rate=[8,0,5,3,9,6]))
+    # print(obj.function(vapor_rate=[8,0,5,3,9,6]))
+    print(obj.function(vapor_rate=[55, -20, 80, -100, -80, 100, -20, 55]))
 
 # Encryption and Decryption
