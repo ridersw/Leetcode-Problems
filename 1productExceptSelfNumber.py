d454f157c644cfaeaafd2eafae69341beef4e3b8
Shashiraj Walsetwar <wshashiraj@gmail.com>
2023-02-24 16:14:44 -0800

    Daily Code

---
+++ 1productExceptSelfNumber.py
@@ -0,0 +1,41 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+        
+        answer[0] = 1
+        
+        for swi in range(1, len(nums)):
+            answer[swi] = answer[swi-1] * nums[swi-1]
+        rightProduct = 1
+        
+        for swi in range(len(nums)-1,-1,-1):
+            answer[swi] = answer[swi] * rightProduct
+            rightProduct *= nums[swi]
+        return answer
+
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 105
+# -30 <= nums[i] <= 30
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+# Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
