37bbc0d0871c7f61d72f11c72015a1f66442e7ad
Shashiraj Walsetwar <wshashiraj@gmail.com>
2023-03-10 13:38:42 -0800

    Daily Code

---
+++ firstUniqChar.py
@@ -0,0 +1,37 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dic = {}
+        
+        for swi in range(len(s)):
+            if s[swi] not in dic:
+                dic[s[swi]] = []
+            dic[s[swi]].append(swi)
+            
+        for key, values in dic.items():
+            if len(values) == 1:
+                return values[0]
+            
+        return -1
+
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+# Example 1:
+
+# Input: s = "leetcode"
+# Output: 0
+# Example 2:
+
+# Input: s = "loveleetcode"
+# Output: 2
+# Example 3:
+
+# Input: s = "aabb"
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists of only lowercase English letters.
