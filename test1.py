267006773c1fbd0cbacac2a7445f098f8a025928
Shashiraj Walsetwar <wshashiraj@gmail.com>
2023-10-24 23:49:04 -0700

    Daily Code

---
+++ test1.py
@@ -0,0 +1,10 @@
+def ManaliSkills():
+    skills = ["SQL", "Python"]
+
+    return skills
+
+Manali = ManaliSkills()
+if "Python" not in Manali:
+    print("Learn Python")
+else:
+    print("She knows Python")
