ac957829760b81aea14a15e25194cff136dd4159
Shashiraj Walsetwar <wshashiraj@gmail.com>
2024-03-25 23:40:42 -0700

    Daily Code

--- firstMissingPositive.py
+++ firstMissingPositive.py
@@ -1,25 +1,37 @@
-def firstMissingPositive(nums):
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        counter = 1
+        
+        while True:
+            if counter not in nums:
+                return counter
+            counter += 1
+            
+# Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
 
-	for swi in range(1, max(nums)+2):
-		#print(f"swi: {swi}")
-		if swi not in nums:
-			return swi
-	
-if __name__ == "__main__":
-	print(firstMissingPositive([1,2,0]))
-	print(firstMissingPositive([3,4,-1,1]))
-	
-#Given an unsorted integer array nums, find the smallest missing positive integer.
+# You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
 
-#Example 1:
+ 
 
-#Input: nums = [1,2,0]
-#Output: 3
-#Example 2:
+# Example 1:
 
-#Input: nums = [3,4,-1,1]
-#Output: 2
-#Example 3:
+# Input: nums = [1,2,0]
+# Output: 3
+# Explanation: The numbers in the range [1,2] are all in the array.
+# Example 2:
 
-#Input: nums = [7,8,9,11,12]
-#Output: 1
+# Input: nums = [3,4,-1,1]
+# Output: 2
+# Explanation: 1 is in the array but 2 is missing.
+# Example 3:
+
+# Input: nums = [7,8,9,11,12]
+# Output: 1
+# Explanation: The smallest positive integer 1 is missing.
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -231 <= nums[i] <= 231 - 1
