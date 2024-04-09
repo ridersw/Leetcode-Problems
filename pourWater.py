c2fe906a8697a9372d77c8c99796a0bad415f85c
Shashiraj Walsetwar <wshashiraj@gmail.com>
2024-04-08 21:06:05 -0700

    Daily Code

---
+++ pourWater.py
@@ -0,0 +1,35 @@
+import math
+
+class Solution:
+    def pourWater(self, heights, volume, k):
+
+        heights = [math.inf] + heights + [math.inf]
+
+        while volume > 0:
+            print(heights[1:-1])
+            if heights[k] < heights[k-1] and heights[k] < heights[k+1]:
+                heights[k] += 1
+
+            else:
+                pointer1 = k-1
+                pointer2 = k+1
+
+                while True:
+                    if heights[pointer1] < heights[pointer2]:
+                        print(f'Found {pointer1} < {pointer2}')
+                        heights[pointer1] += 1
+                        break
+                    elif heights[pointer1] > heights[pointer2]:
+                        print(f'Found {pointer1} > {pointer2}')
+                        heights[pointer2] += 1
+                    else:
+                        print('else')
+                        pointer1 -= 1
+                        pointer2 += 1 
+            volume -= 1
+
+        return heights[1:-1]
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.pourWater(heights = [2,1,1,2,1,2,2], volume = 4, k = 3))
