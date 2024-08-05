71084d92d8ce076cb776713e28dc9a822e8cac25
Shashiraj Walsetwar <wshashiraj@gmail.com>
2024-08-05 01:54:22 -0700

    Daily Code

---
+++ canAttendMeetings.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+
+        for swi in range(len(intervals)-1):
+            if intervals[swi][1] > intervals[swi+1][0]:
+                return False
+
+        return True
+
+# Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+ 
+
+# Example 1:
+
+# Input: intervals = [[0,30],[5,10],[15,20]]
+# Output: false
+# Example 2:
+
+# Input: intervals = [[7,10],[2,4]]
+# Output: true
+ 
+
+# Constraints:
+
+# 0 <= intervals.length <= 104
+# intervals[i].length == 2
+# 0 <= starti < endi <= 106
