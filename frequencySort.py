31313da72da5a55e82c65672fd175196ef23fc07
Shashiraj Walsetwar <wshashiraj@gmail.com>
2024-10-16 21:46:48 -0700

    Daily Code

--- frequencySort.py
+++ frequencySort.py
@@ -1,69 +1,34 @@
-import collections
+from collections import Counter
 
 class Solution:
-    def frequencySort(self, s: str):
-        dict = {}
-        
-        for char in s:
-            if char.lower() in dict:
-                dict[char] += 1
-            else:
-                dict[char] = 1
-                
-        array = []
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = Counter(nums)
 
-        for key, values in dict.items():
-            array.append([key, values])
+        return sorted(nums, key = lambda x:(freq[x], -x))
 
-        array = sorted(array, key=lambda x:x[1], reverse=True)
+# Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
 
-        res = ""
-
-        for chars in array:
-            res += chars[0] * chars[1]
-
-        return res
-
-
-
-if __name__ == "__main__":
-    obj = Solution()
-
-    # s = "tree"
-    # s = "cccaaa"
-    # s = "Aabb"
-
-    print(obj.frequencySort(s = "tree"))
-    print(obj.frequencySort(s = "cccaaa"))
-    print(obj.frequencySort(s = "Aabb"))
-
-# Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
-
-# Return the sorted string. If there are multiple answers, return any of them.
+# Return the sorted array.
 
  
 
 # Example 1:
 
-# Input: s = "tree"
-# Output: "eert"
-# Explanation: 'e' appears twice while 'r' and 't' both appear once.
-# So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+# Input: nums = [1,1,2,2,2,3]
+# Output: [3,1,1,2,2,2]
+# Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
 # Example 2:
 
-# Input: s = "cccaaa"
-# Output: "aaaccc"
-# Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
-# Note that "cacaca" is incorrect, as the same characters must be together.
+# Input: nums = [2,3,1,3,2]
+# Output: [1,3,3,2,2]
+# Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
 # Example 3:
 
-# Input: s = "Aabb"
-# Output: "bbAa"
-# Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
-# Note that 'A' and 'a' are treated as two different characters.
+# Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+# Output: [5,-1,4,4,-6,-6,1,1,1]
  
 
 # Constraints:
 
-# 1 <= s.length <= 5 * 105
-# s consists of uppercase and lowercase English letters and digits.
+# 1 <= nums.length <= 100
+# -100 <= nums[i] <= 100
